063d703f7dd5a5d17eaed0752065c5582ac644c1
Suraj <sthanugundla@ufl.edu>
2018-09-05 11:29:20 -0400

    Fixed Task.await bug

--- proj1.exs
+++ proj1.exs
@@ -1,34 +1,24 @@
 [n, k] = System.argv
 {n, _} = Integer.parse(n)
-{k, _}= Integer.parse(k)
+{k, _} = Integer.parse(k)
 
+work_unit = 100
 
-worker_unit = 45
-{:ok, counter_pid} = Counter.start_link([])
+:erlang.statistics(:runtime)
+:erlang.statistics(:wall_clock)
 
-start_list = :lists.seq(1, n, worker_unit)
+{:ok, agent} = Counter.start_link([])
 
-wrapper = fn (start_list, k, worker_unit, counter_pid) ->
-  Enum.each start_list, fn start ->
-    #TODO: Have to increase timeout for larger numbers
-    Task.async(Worker, :run, [[k, start, worker_unit, counter_pid]]) |> Task.await
-  end
-end
+start_points = :lists.seq(1, n, work_unit)
 
-{time, _} = :timer.tc(wrapper, [start_list, k, worker_unit, counter_pid])
+tasks = Enum.map(start_points, &Task.async(Worker, :run, [[k, &1, work_unit, agent]]))
+Enum.map(tasks, &Task.await(&1, 500000)) # Large timeout
 
-# Enum.each start_list, fn start ->
-#   #TODO: Have to increase timeout for larger numbers
-#   Task.async(Worker, :run, [[k, start, worker_unit, counter_pid]]) |> Task.await
-# end
+{_, t1} = :erlang.statistics(:runtime) # CPU time
+{_, t2} = :erlang.statistics(:wall_clock) # Real time
 
-output_list = Counter.get(counter_pid)
+result = Counter.get(agent)
+sorted_list = Enum.sort(result)
+Enum.each sorted_list, &IO.puts(&1)
 
-
-final_output = Enum.sort(output_list)
-
-Enum.each final_output, fn i ->
-  IO.puts(i)
-end
-
-IO.puts("Time: #{time/1000} milliseconds")
+IO.puts("CPU time: #{t1} ms Real time: #{t2} ms Time ratio: #{t1/t2}")
